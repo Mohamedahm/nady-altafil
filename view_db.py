723c12d3e4159a7acf3047ff1adda08d4be33c52
Mohamed ALHosani <mohamed_31383@hotmail.com>
2025-07-19 15:32:30 +0400

    Remove ignored files from repository

--- view_db.py
+++
@@ -1,28 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect("database.db")
-c = conn.cursor()
-
-c.execute("SELECT * FROM subscribers")
-rows = c.fetchall()
-
-print("المشتركين:\n")
-for row in rows:
-    print(f"ID: {row[0]}, Name: {row[1]}, Email: {row[2]}, Start Date: {row[3]}, Paid: {row[4]}, Payment Email Sent: {row[5]}")
-
-conn.close()
-
-
-import sqlite3
-
-conn = sqlite3.connect("database.db")
-c = conn.cursor()
-
-c.execute("SELECT * FROM subscribers")
-rows = c.fetchall()
-
-print("المشتركين:\n")
-for row in rows:
-    print(f"🧾 الصف الكامل: {row}")  # نطبع كل الصف كما هو
-
-conn.close()
